55d15204a2df8c4f43e0eda6ca94581a03363308
Arthur H. Neto <henkels.arthur@gmail.com>
2026-08-29 21:20:11 -0300

    feat: missing file flags

--- src/linux.c
+++ src/linux.c
@@ -6,11 +6,26 @@
 ////////////////////////////////////////////////////
 // file flags
 
-#define O_RDONLY 0x0000
-#define O_WRONLY 0x0001
-#define O_RDWR   0x0002
-#define O_CREAT  0x0040
-#define O_TRUNC  0x0200
+#define O_RDONLY    0x0000
+#define O_WRONLY    0x0001
+#define O_RDWR      0x0002
+#define O_CREAT     0x0040
+#define O_EXCL      0x0080
+#define O_NOCTTY    0x0100
+#define O_TRUNC     0x0200
+#define O_APPEND    0x0400
+#define O_NONBLOCK  0x0800
+#define O_DSYNC     0x1000
+#define O_ASYNC     0x2000
+#define O_DIRECT    0x4000
+#define O_LARGEFILE 0x8000
+#define O_DIRECTORY 0x10000
+#define O_NOFOLLOW  0x20000
+#define O_NOATIME   0x40000
+#define O_CLOEXEC   0x80000
+#define O_SYNC      0x101000
+#define O_PATH      0x200000
+#define O_TMPFILE   (0x400000 | O_DIRECTORY)
 
 #define SEEK_SET 0
 #define SEEK_CUR 1
