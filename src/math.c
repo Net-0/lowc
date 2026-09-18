14bfd84e75e0ffe546907dce4f10c412137fa069
Net-0 <90418721+Net-0@users.noreply.github.com>
2026-09-18 17:15:25 -0300

    feat: math

---
+++ src/math.c
@@ -0,0 +1,10 @@
+#ifndef MATH_C
+#define MATH_C
+
+// Return the greater of a and b
+#define max(a, b) ((a) > (b) ? (a) : (b))
+
+// Return the lesser of a and b
+#define min(a, b) ((a) < (b) ? (a) : (b))
+
+#endif
