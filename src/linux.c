f405c25f8e451bff648ef39b5de294cb9ebe1795
Net-0 <90418721+Net-0@users.noreply.github.com>
2026-09-18 17:15:25 -0300

    feat: open, close and lseek syscalls

--- src/linux.c
+++ src/linux.c
@@ -3,6 +3,43 @@
 
 #include "./types.c"
 
+////////////////////////////////////////////////////
+// file flags
+
+#define O_RDONLY 0x0000
+#define O_WRONLY 0x0001
+#define O_RDWR   0x0002
+#define O_CREAT  0x0040
+#define O_TRUNC  0x0200
+
+#define SEEK_SET 0
+#define SEEK_CUR 1
+#define SEEK_END 2
+
+////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////
+// mmap-family flags
+
+#define PROT_NONE  0x0
+#define PROT_READ  0x1
+#define PROT_WRITE 0x2
+#define PROT_EXEC  0x4
+
+#define MAP_SHARED    0x01
+#define MAP_PRIVATE   0x02
+#define MAP_FIXED     0x10
+#define MAP_ANONYMOUS 0x20
+
+#define MREMAP_MAYMOVE 0x1
+#define MREMAP_FIXED   0x2
+
+#define MS_ASYNC      0x1
+#define MS_INVALIDATE 0x2
+#define MS_SYNC       0x4
+
+////////////////////////////////////////////////////
+
 // Linux 'read' syscall
 isize read(const i32 fileDescriptor, const byte* buffer, const usize count) {
   isize ret;
@@ -22,6 +59,42 @@ isize write(const i32 fileDescriptor, const byte* buffer, const usize count) {
     "syscall"
     : "=a"(ret)
     : "a"(1), "D"(fileDescriptor), "S"(buffer), "d"(count)
+    : "rcx", "r11", "memory"
+  );
+  return ret;
+}
+
+// Linux 'open' syscall
+isize open(const byte* path, const i32 flags, const i32 mode) {
+  isize ret;
+  __asm__ volatile (
+    "syscall"
+    : "=a"(ret)
+    : "a"(2), "D"(path), "S"(flags), "d"(mode)
+    : "rcx", "r11", "memory"
+  );
+  return ret;
+}
+
+// Linux 'close' syscall
+isize close(const i32 fileDescriptor) {
+  isize ret;
+  __asm__ volatile (
+    "syscall"
+    : "=a"(ret)
+    : "a"(3), "D"(fileDescriptor)
+    : "rcx", "r11", "memory"
+  );
+  return ret;
+}
+
+// Linux 'lseek' syscall
+isize lseek(const i32 fileDescriptor, const isize offset, const i32 whence) {
+  isize ret;
+  __asm__ volatile (
+    "syscall"
+    : "=a"(ret)
+    : "a"(8), "D"(fileDescriptor), "S"(offset), "d"(whence)
     : "rcx", "r11", "memory"
   );
   return ret;
@@ -52,26 +125,6 @@ isize pwrite(const i32 fileDescriptor, const byte* buffer, const usize count, co
   );
   return ret;
 }
-
-////////////////////////////////////////////////////
-// mmap-family flags
-
-#define PROT_NONE  0x0
-#define PROT_READ  0x1
-#define PROT_WRITE 0x2
-#define PROT_EXEC  0x4
-
-#define MAP_SHARED    0x01
-#define MAP_PRIVATE   0x02
-#define MAP_FIXED     0x10
-#define MAP_ANONYMOUS 0x20
-
-#define MREMAP_MAYMOVE 0x1
-#define MREMAP_FIXED   0x2
-
-#define MS_ASYNC      0x1
-#define MS_INVALIDATE 0x2
-#define MS_SYNC       0x4
 
 // Linux 'mmap' syscall
 isize mmap(byte* addr, const usize length, const i32 prot, const i32 flags, const i32 fileDescriptor, const isize offset) {
@@ -136,8 +189,6 @@ isize msync(byte* addr, const usize length, const i32 flags) {
   );
   return ret;
 }
-
-////////////////////////////////////////////////////
 
 // Linux 'exit' syscall
 void __attribute__((noreturn)) exit(const u8 code) {
